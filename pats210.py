aa389042c45410f3f6ab563c1ca88ca1fee523e3
gpatiniotis <gpatiniotis85@gmail.com>
2017-10-09 23:39:14 +0300

    Add files via upload

---
+++ pats210.py
@@ -0,0 +1,23 @@
+li=[]
+d=24
+max4=0
+max5=0
+max6=0
+max7=0
+li = raw_input('doste tin lista ')
+li = [int(x) for x in li.split(',')]
+for i in range(d):
+    if ( i < 6 ) :
+        if (li[i]> max4):
+            max4=li[i]
+    elif ( i < 12 ) :
+        if (li[i] > max5):
+            max5=li[i]
+    elif ( i < 18 ) :
+        if ( li[i] > max6 ):
+            max6 = li[i]
+            
+    else:
+        if (li[i]>max7):
+            max7=li[i]
+print "[('4:00pm', %d),('5:00pm', %d),('6:00pm', %d),('7:00pm', %d)]" %(max4 ,max5 ,max6 ,max7)
